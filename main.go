9219a12d1bfd7828f5c1d1d656b0ee00c3f0df21
Niklas Rittmann <niklas.rittmann@capgemini.com>
2023-04-12 21:52:50 +0200

    chore: Cleanup and readme update

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"neural-go/net"
 )
 
 func main() {
-
-	fn := net.ActPersistMap["neural-go/net.relu"]
-	log.Printf("%v", fn(-51))
 
 	layers := []net.LayerDef{
 		{Input_dim: 2, Output_dim: 25, Activation: net.ReLU},
@@ -39,13 +33,8 @@ func main() {
 		{1, 1},
 	}.Transpose()
 	model := net.NewNet(0.5, net.MSE, layers)
-	// Persist
-	file, _ := json.MarshalIndent(model, "", " ")
-	_ = ioutil.WriteFile("test.json", file, 0644)
-
-	newNet := net.NewNetFromFile("test.json")
-
-	newNet.Train(x, y, 10)
+	model.Train(x, y, 10)
+	model.ToFile("2layer.json")
 	fmt.Printf("Expected Preds %v\n", y)
-	newNet.Predict(x)
+	model.Predict(x)
 }
